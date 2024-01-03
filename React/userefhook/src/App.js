7f341492c79a2c86e420143611a8efee17d00662
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2024-01-03 15:11:07 +0530

    Add src folder

---
+++ React/userefhook/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import FocusInput from './Components/FocusInput';
+import ClassTimer from './Components/ClassTimer';
+import HookTimer from './Components/HookTimer';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FocusInput /> */}
+      {/* <ClassTimer /> */}
+      <HookTimer />
+    </div>
+  );
+}
+
+export default App;
